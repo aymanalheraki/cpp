deda2898e4934e3a4b017980b1d99c1579e32630
Ayman Alheraki <sug@astorm.net>
2023-11-17 23:36:32 +0300

    First commit

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 //
 #include <iostream>
 #include "main.h"
-
+int gg();
 int main() {
     std::cout << "Hello World" << std::endl;
+    gg();
     return 0;
+}
+
+int gg() {
+    std::cout << "fdsfsdfds";
 }
