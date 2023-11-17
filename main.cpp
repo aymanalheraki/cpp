46945c3a9b8487b2d9e66dbc8bc98093725d8338
Ayman Alheraki <sug@astorm.net>
2023-11-17 23:29:40 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by X1 on 17-11-2023.
+//
+#include <iostream>
+#include "main.h"
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by X1 on 17-11-2023.
+//
+
+#ifndef PRO2_MAIN_H
+#define PRO2_MAIN_H
+
+#endif //PRO2_MAIN_H
